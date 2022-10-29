238ea5668463b837209c2f3e936052c749dd2d30
ARUN KUMAR YADAV <77447025+Arun12116@users.noreply.github.com>
2022-10-29 14:28:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Project1 from './Project1';
+import Project2 from './Project2';
+
+function App() {
+  return (
+   <>
+   <Project1/>
+   <Project2/>
+   </>
+  );
+}
+
+export default App;
---
+++ src/Project2.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Project2.css"
+const Project2 = () => {
+  return (
+    <>
+    <div className="header">
+        <div className="box">
+            <h1>The Evoloution of Airbnb's Frontend </h1>
+            
+            <h2>@spikebrehm</h2>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Project2
